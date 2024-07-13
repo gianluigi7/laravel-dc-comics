cd01f8ce8e5ea4a55b3842712209dcb8bad63b8e
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-07-13 17:42:31 +0200

    create fixed

--- app/Http/Controllers/Admin/ComicsController.php
+++ app/Http/Controllers/Admin/ComicsController.php
@@ -31,7 +31,23 @@ class ComicsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+
+        $comic = new Comic();
+
+            $comic->title = $data['title'];
+            $comic->description = $data['description'];
+            $comic->thumb = $data['thumb'];
+            $comic->price = $data['price'];
+            $comic->series = $data['series'];
+            $comic->sale_date = $data['sale_date'];
+            $comic->type = $data['type'];
+            $comic->artists = $data['artists'];
+            $comic->writers = $data['writers'];
+
+        $comic->save();
+
+        return redirect()->route('comics.show', $comic->id);
     }
 
     /**
@@ -56,23 +72,7 @@ class ComicsController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        $data = $request->all();
-
-        $comic = new Comic();
-
-            $comic->title = $data['title'];
-            $comic->description = $data['description'];
-            $comic->thumb = $data['thumb'];
-            $comic->price = $data['price'];
-            $comic->series = $data['series'];
-            $comic->sale_date = $data['sale_date'];
-            $comic->type = $data['type'];
-            $comic->artists = $data['artists'];
-            $comic->writers = $data['writers'];
-
-        $comic->save();
-
-        return redirect()->route('comics.index', $comic->id);
+        
     }
 
     /**
